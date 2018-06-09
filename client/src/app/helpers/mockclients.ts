bcc7192fd87554d865e98f0154f0bb066661d2ba
Aleksandr Siryi <theobroma222@gmail.com>
2018-06-10 00:48:09 +0300

    initial

---
+++ client/src/app/helpers/mockclients.ts
@@ -0,0 +1,77 @@
+//mock data if needed
+const clientsarr = [
+  {
+    _id: '5a07bc6ba958450408b4e74e',
+    name: 'Svetlana Sorokovnikova',
+    discountType: 'silver',
+    discountSum: 221,
+    discountProcent: 18,
+    gender: 'female'
+  },
+  {
+    _id: '5a07bc6ba958450408b4e74e',
+    name: 'Sergei Pluhin',
+    discountType: 'gold',
+    discountSum: 645,
+    discountProcent: 17,
+    gender: 'male'
+  },
+  {
+    _id: '5a07bc6ba958450408b4e74e',
+    name: 'Greta Holodnaia',
+    discountType: 'wood',
+    discountSum: 320,
+    discountProcent: 10,
+    gender: 'female'
+  },
+  {
+    _id: '5a07bc6ba958450408b4e74e',
+    name: 'Andrei Stelia',
+    discountType: 'silver',
+    discountSum: 124,
+    discountProcent: 11,
+    gender: 'male'
+  },
+  {
+    _id: '5a07bc6ba958450408b4e74e',
+    name: 'Alex Bulavka',
+    discountType: 'wood',
+    discountSum: 101,
+    discountProcent: 8,
+    gender: 'male'
+  },
+  {
+    _id: '5a07bc6ba958450408b4e74e',
+    name: 'Igor Shumilovec',
+    discountType: 'silver',
+    discountSum: 44,
+    discountProcent: 23,
+    gender: 'female'
+  },
+  {
+    _id: '5a07bc6ba958450408b4e74e',
+    name: 'Semen Bluher',
+    discountType: 'silver',
+    discountSum: 543,
+    discountProcent: 15,
+    gender: 'male'
+  },
+  {
+    _id: '5a07bc6ba958450408b4e74e',
+    name: 'Irina Chernisheva',
+    discountType: 'silver',
+    discountSum: 126,
+    discountProcent: 9,
+    gender: 'female'
+  },
+  {
+    _id: '5a07bc6ba958450408b4e74e',
+    name: 'Dasha Savelieva',
+    discountType: 'wood',
+    discountSum: 221,
+    discountProcent: 18,
+    gender: 'female'
+  }
+];
+
+export default clientsarr;

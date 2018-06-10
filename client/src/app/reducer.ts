6e2d67bac3b2c3611af1054318c8781e56474923
Aleksandr Siryi <theobroma222@gmail.com>
2018-06-10 19:03:53 +0300

    table-row hover

--- client/src/app/reducer.ts
+++ client/src/app/reducer.ts
@@ -1,17 +1,8 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Action, AnyAction } from 'redux';
-import { Todo, IState } from './model';
 //mock todos reducer with data
 import mockClients from './helpers/mockclients';
 import { TOGGLE_FILTER, FETCH_CLIENTS_REQUEST } from './actions';
-
-// //filter reducer
-// function filter(state = 'SHOW_ALL', action: any) {
-//   if (action.type === SET_FILTER) {
-//     return action.filter;
-//   }
-//   return state;
-// }
 
 const filtersInitialState = {
   data: [
@@ -38,35 +29,6 @@ function filters(state = filtersInitialState, action: any) {
   return state;
 }
 
-// case 'GOLD':
-// return {
-//   ...state,
-//   data: state.data.map((filter:any) => {
-//     if (filter.name === action.filter.toLowerCase()) {
-//       return { ...todo, completed: !todo.completed };
-//     }
-//     return todo;
-//   }),
-// };
-// case 'SILVER':
-// return {
-//   ...state,
-//   silver: !state.silver
-// };
-// case 'WOOD':
-// return {
-//   ...state,
-//   wood: !state.wood
-// };
-// case 'STANDART':
-// return {
-//   ...state,
-//   standart: !state.standart
-// };
-
-//data from file
-const DefaultState: any = mockClients;
-
 const clientsInitialState = {
   data: mockClients,
   pending: false,
@@ -83,21 +45,6 @@ function clients(state = clientsInitialState, action: any) {
       return state;
   }
 }
-
-//epic for testing
-// const users = (state = {}, action: any) => {
-//   switch (action.type) {
-//     case FETCH_USER_FULFILLED:
-//       return {
-//         ...state,
-//         // `login` is the username
-//         [action.payload.login]: action.payload
-//       };
-
-//     default:
-//       return state;
-//   }
-// };
 
 export default combineReducers({
   filters,

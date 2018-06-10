2f4cc9415827eaf57431841dfb65b413878d6ce8
Aleksandr Siryi <theobroma222@gmail.com>
2018-06-10 18:21:06 +0300

    filters reducer

--- client/src/app/reducer.ts
+++ client/src/app/reducer.ts
@@ -44,34 +44,45 @@ const filtersInitialState = {
 
 //filter reducer
 function filters(state = filtersInitialState, action: any) {
-  // if (action.type === TOGGLE_FILTER) {
-  //   switch (action.filter) {
-  //     case 'GOLD':
-  //       return {
-  //         ...state,
-  //         gold: !state.gold
-  //       };
-  //     case 'SILVER':
-  //       return {
-  //         ...state,
-  //         silver: !state.silver
-  //       };
-  //     case 'WOOD':
-  //       return {
-  //         ...state,
-  //         wood: !state.wood
-  //       };
-  //     case 'STANDART':
-  //       return {
-  //         ...state,
-  //         standart: !state.standart
-  //       };
-  //     default:
-  //       return state;
-  //   }
-  // }
+  if (action.type === TOGGLE_FILTER) {
+    return {
+      ...state,
+      data: state.data.map((filter: any) => {
+        if (filter.name === action.filter.toLowerCase()) {
+          return { ...filter, active: !filter.active };
+        }
+        return filter;
+      })
+    };
+  }
   return state;
 }
+
+// case 'GOLD':
+// return {
+//   ...state,
+//   data: state.data.map((filter:any) => {
+//     if (filter.name === action.filter.toLowerCase()) {
+//       return { ...todo, completed: !todo.completed };
+//     }
+//     return todo;
+//   }),
+// };
+// case 'SILVER':
+// return {
+//   ...state,
+//   silver: !state.silver
+// };
+// case 'WOOD':
+// return {
+//   ...state,
+//   wood: !state.wood
+// };
+// case 'STANDART':
+// return {
+//   ...state,
+//   standart: !state.standart
+// };
 
 //data from file
 const DefaultState: any = mockClients;
